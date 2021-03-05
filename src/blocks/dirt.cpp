c853863c335c93430e65a2907d57229558d5d0af
vakus <u0001@mmaps.org>
2021-03-05 17:52:32 +0000

    Improved rendering mesh

--- src/blocks/dirt.cpp
+++ src/blocks/dirt.cpp
@@ -17,54 +17,105 @@ public:
     GameObject3D getMesh(int FaceFlags) override
     {
         GameObject3D gameObject{
-            {/*back*/
-             {{0.0f, 0.0f, 1.0f}, getTextureCoord(0, 0, TextureCoordType::TOP_LEFT), BLOCK_SHADE_TOP},
-             {{1.0f, 0.0f, 1.0f}, getTextureCoord(0, 0, TextureCoordType::BOTTOM_LEFT), BLOCK_SHADE_TOP},
-             {{1.0f, 1.0f, 1.0f}, getTextureCoord(0, 0, TextureCoordType::BOTTOM_RIGHT), BLOCK_SHADE_TOP},
-             {{0.0f, 1.0f, 1.0f}, getTextureCoord(0, 0, TextureCoordType::TOP_RIGHT), BLOCK_SHADE_TOP},
-             /*bottom*/
-             {{0.0f, 0.0f, 1.0f}, getTextureCoord(0, 0, TextureCoordType::TOP_RIGHT), BLOCK_SHADE_SIDE},
-             {{1.0f, 0.0f, 1.0f}, getTextureCoord(0, 0, TextureCoordType::BOTTOM_RIGHT), BLOCK_SHADE_SIDE},
-             {{0.0f, 0.0f, 0.0f}, getTextureCoord(0, 0, TextureCoordType::TOP_LEFT), BLOCK_SHADE_SIDE},
-             {{1.0f, 0.0f, 0.0f}, getTextureCoord(0, 0, TextureCoordType::BOTTOM_LEFT), BLOCK_SHADE_SIDE},
-             /*top*/
-             {{0.0f, 1.0f, 1.0f}, getTextureCoord(0, 0, TextureCoordType::TOP_RIGHT), BLOCK_SHADE_SIDE},
+            {},
+            {}
+            };
+
+        uint base = 0;
+        if((FaceFlags & BlockFace::BACK) == BlockFace::BACK){
+            // dont add base here; it always will be 0
+            gameObject.verticies.insert(gameObject.verticies.end(), {
+             {{0.0f, 0.0f, 1.0f}, getTextureCoord(0, 0, TextureCoordType::TOP_LEFT), BLOCK_SHADE_SIDE},
+             {{1.0f, 0.0f, 1.0f}, getTextureCoord(0, 0, TextureCoordType::BOTTOM_LEFT), BLOCK_SHADE_SIDE},
              {{1.0f, 1.0f, 1.0f}, getTextureCoord(0, 0, TextureCoordType::BOTTOM_RIGHT), BLOCK_SHADE_SIDE},
-             {{0.0f, 1.0f, 0.0f}, getTextureCoord(0, 0, TextureCoordType::TOP_LEFT), BLOCK_SHADE_SIDE},
-             {{1.0f, 1.0f, 0.0f}, getTextureCoord(0, 0, TextureCoordType::BOTTOM_LEFT), BLOCK_SHADE_SIDE},
-             /*left*/
+             {{0.0f, 1.0f, 1.0f}, getTextureCoord(0, 0, TextureCoordType::TOP_RIGHT), BLOCK_SHADE_SIDE},
+            });
+            gameObject.indicies.insert(gameObject.indicies.end(), {0, 1, 3, 1, 2, 3});
+            base += 4;
+        }
+        if((FaceFlags & BlockFace::BOTTOM) == BlockFace::BOTTOM){
+            gameObject.verticies.insert(gameObject.verticies.end(), {
+             {{0.0f, 0.0f, 1.0f}, getTextureCoord(0, 0, TextureCoordType::TOP_RIGHT), BLOCK_SHADE_BOTTOM},
+             {{1.0f, 0.0f, 1.0f}, getTextureCoord(0, 0, TextureCoordType::BOTTOM_RIGHT), BLOCK_SHADE_BOTTOM},
+             {{0.0f, 0.0f, 0.0f}, getTextureCoord(0, 0, TextureCoordType::TOP_LEFT), BLOCK_SHADE_BOTTOM},
+             {{1.0f, 0.0f, 0.0f}, getTextureCoord(0, 0, TextureCoordType::BOTTOM_LEFT), BLOCK_SHADE_BOTTOM},
+            });
+            gameObject.indicies.insert(gameObject.indicies.end(), {
+                base + 2,
+                base + 1,
+                base, // +0
+                base + 2,
+                base + 3,
+                base + 1
+            });
+            base += 4;
+        }
+        if((FaceFlags & BlockFace::TOP) == BlockFace::TOP){
+            gameObject.verticies.insert(gameObject.verticies.end(), {
+             {{0.0f, 1.0f, 1.0f}, getTextureCoord(0, 0, TextureCoordType::TOP_RIGHT), BLOCK_SHADE_TOP},
+             {{1.0f, 1.0f, 1.0f}, getTextureCoord(0, 0, TextureCoordType::BOTTOM_RIGHT), BLOCK_SHADE_TOP},
+             {{0.0f, 1.0f, 0.0f}, getTextureCoord(0, 0, TextureCoordType::TOP_LEFT), BLOCK_SHADE_TOP},
+             {{1.0f, 1.0f, 0.0f}, getTextureCoord(0, 0, TextureCoordType::BOTTOM_LEFT), BLOCK_SHADE_TOP},
+            });
+            gameObject.indicies.insert(gameObject.indicies.end(), {
+                base + 3,
+                base + 2,
+                base + 1,
+                base + 2,
+                base, // +0
+                base + 1
+            });
+            base += 4;
+        }
+        if((FaceFlags & BlockFace::LEFT) == BlockFace::LEFT){
+            gameObject.verticies.insert(gameObject.verticies.end(), {
              {{1.0f, 0.0f, 1.0f}, getTextureCoord(0, 0, TextureCoordType::TOP_RIGHT), BLOCK_SHADE_SIDE},
              {{1.0f, 1.0f, 1.0f}, getTextureCoord(0, 0, TextureCoordType::BOTTOM_RIGHT), BLOCK_SHADE_SIDE},
              {{1.0f, 0.0f, 0.0f}, getTextureCoord(0, 0, TextureCoordType::TOP_LEFT), BLOCK_SHADE_SIDE},
              {{1.0f, 1.0f, 0.0f}, getTextureCoord(0, 0, TextureCoordType::BOTTOM_LEFT), BLOCK_SHADE_SIDE},
-             /*right*/
+            });
+            gameObject.indicies.insert(gameObject.indicies.end(), {
+                base + 2,
+                base + 1,
+                base, // +0
+                base + 2,
+                base + 3,
+                base + 1
+            });
+            base += 4;
+        }
+        if((FaceFlags & BlockFace::RIGHT) == BlockFace::RIGHT){
+            gameObject.verticies.insert(gameObject.verticies.end(), {
              {{0.0f, 0.0f, 1.0f}, getTextureCoord(0, 0, TextureCoordType::TOP_RIGHT), BLOCK_SHADE_SIDE},
              {{0.0f, 1.0f, 1.0f}, getTextureCoord(0, 0, TextureCoordType::BOTTOM_RIGHT), BLOCK_SHADE_SIDE},
              {{0.0f, 0.0f, 0.0f}, getTextureCoord(0, 0, TextureCoordType::TOP_LEFT), BLOCK_SHADE_SIDE},
              {{0.0f, 1.0f, 0.0f}, getTextureCoord(0, 0, TextureCoordType::BOTTOM_LEFT), BLOCK_SHADE_SIDE},
-             /*front*/
-             {{0.0f, 0.0f, 0.0f}, getTextureCoord(0, 0, TextureCoordType::TOP_LEFT), BLOCK_SHADE_BOTTOM},
-             {{1.0f, 0.0f, 0.0f}, getTextureCoord(0, 0, TextureCoordType::BOTTOM_LEFT), BLOCK_SHADE_BOTTOM},
-             {{1.0f, 1.0f, 0.0f}, getTextureCoord(0, 0, TextureCoordType::BOTTOM_RIGHT), BLOCK_SHADE_BOTTOM},
-             {{0.0f, 1.0f, 0.0f}, getTextureCoord(0, 0, TextureCoordType::TOP_RIGHT), BLOCK_SHADE_BOTTOM}},
-            {}};
-        if((FaceFlags & BlockFace::BACK) == BlockFace::BACK){
-            gameObject.indicies.insert(gameObject.indicies.end(), {0, 1, 3, 1, 2, 3});
-        }
-        if((FaceFlags & BlockFace::BOTTOM) == BlockFace::BOTTOM){
-            gameObject.indicies.insert(gameObject.indicies.end(), {6, 5, 4, 6, 7, 5});
-        }
-        if((FaceFlags & BlockFace::TOP) == BlockFace::TOP){
-            gameObject.indicies.insert(gameObject.indicies.end(), {11, 10, 9, 10, 8, 9});
-        }
-        if((FaceFlags & BlockFace::LEFT) == BlockFace::LEFT){
-            gameObject.indicies.insert(gameObject.indicies.end(), {14, 13, 12, 14, 15, 13});
-        }
-        if((FaceFlags & BlockFace::RIGHT) == BlockFace::RIGHT){
-            gameObject.indicies.insert(gameObject.indicies.end(), {16, 17, 18, 17, 19, 18});
+            });
+            gameObject.indicies.insert(gameObject.indicies.end(), {
+                base, // +0
+                base + 1,
+                base + 2,
+                base + 1,
+                base + 3,
+                base + 2
+            });
+            base += 4;
         }
         if((FaceFlags & BlockFace::FRONT) == BlockFace::FRONT){
-            gameObject.indicies.insert(gameObject.indicies.end(), {20, 22, 21, 23, 22, 20});
+            gameObject.verticies.insert(gameObject.verticies.end(), {
+             {{0.0f, 0.0f, 0.0f}, getTextureCoord(0, 0, TextureCoordType::TOP_LEFT), BLOCK_SHADE_SIDE},
+             {{1.0f, 0.0f, 0.0f}, getTextureCoord(0, 0, TextureCoordType::BOTTOM_LEFT), BLOCK_SHADE_SIDE},
+             {{1.0f, 1.0f, 0.0f}, getTextureCoord(0, 0, TextureCoordType::BOTTOM_RIGHT), BLOCK_SHADE_SIDE},
+             {{0.0f, 1.0f, 0.0f}, getTextureCoord(0, 0, TextureCoordType::TOP_RIGHT), BLOCK_SHADE_SIDE}
+            });
+            gameObject.indicies.insert(gameObject.indicies.end(), {
+                base, //+0
+                base + 2,
+                base + 1,
+                base + 3,
+                base + 2,
+                base //+0
+            });
         }
         return gameObject;
     }
